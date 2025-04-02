e6365441dc3e124924a9058591fc519a096ed436
Ghust <andregustavoteixeira58@gmail.com>
2025-04-02 20:04:41 -0300

    routes atualizados

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const express = require('express')
+const app = express()
 const cors = require('cors')
 const db = require('./src/db/db')
+const userRoutes = require('./src/routes/users')
 require('dotenv').config()
 
 const PORT = process.env.PORT || 3000
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
-
 app.use(cors())
+app.use('/api/users',userRoutes)
 
 app.get('/',(req,res)=>{
     res.json({message:"API FUNCIONANDO!"})
@@ -17,10 +19,10 @@ app.get('/',(req,res)=>{
 app.get('/test-db',async (req,res)=>{
     try{
         const result = await db.query('select now()')
-        res.json({sucess:true, serverTime:result.rows[0].now})
+        res.json({success:true, serverTime:result.rows[0].now})
     }catch (err){
         console.log('error ao executar a query.',err)
-        res.status(500).json({sucess:false, error:"Não foi possivel se conectar ao banco de dados."})
+        res.status(500).json({success:false, error:"Não foi possivel se conectar ao banco de dados."})
     }
 })
 
--- src/routes/users.js
+++ src/routes/users.js
@@ -2,6 +2,8 @@ const express = require('express')
 const router = express.Router()
 const bcrypt = require('bcryptjs')
 const db = require('../db/db')
+const jwt = require('jsonwebtoken')
+
 
 router.post('/register',async (req,res)=>{
     try{
@@ -19,16 +21,58 @@ router.post('/register',async (req,res)=>{
         returning id, username, email;        
         `
 
-        const values = [username, email, password]
+        const values = [username, email, hashedPassword]
 
         const result = await db.query(queryText, values)
 
         return res.status(201).json(result.rows[0])
 
     }catch (err){
-        console.error("Error registering user",error)
+        console.error("Error registering user",err)
         res.status(500).json({error:"Server error"})
     }
+})
+
+router.post('/login',async(req,res)=>{
+    try{
+        const {email,password} = req.body
+        if(!email || !password){
+            return res.status(400).json({error:"missing required fields"})
+        }
+
+        const userQuery = `select * from users where email = $1`
+        const userResult = await db.query(userQuery,[email])
+        console.log(userResult)
+        if (userResult.rows.length === 0){
+            return res.status(401).json({error:"invalid email or password."})
+        }
+
+        const user = userResult.rows[0]
+        console.log(user)
+        const isMatch = await bcrypt.compare(password,user.password)
+        if(!isMatch){
+            return res.status(401).json({error:"invalid email or password."})
+        }
+
+        const token = jwt.sign({userId:user.id, email:user.email},
+            process.env.JWT_SECRET,
+            {expiresIn:'2h'}
+        )
+
+        res.json({
+            message:"Login successful",
+            token:token,
+            user:{
+                id:user.id,
+                username:user.username,
+                email:user.email
+            }
+        })
+    }catch (err){
+        console.error("Error during login",err)
+        res.status(500).json({error:"Server Error."})
+    }
+
 })
 
 module.exports = router
