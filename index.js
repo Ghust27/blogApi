45988bd217278549ac2f121c38346faa6d0c70fe
André <166322686+Ghust27@users.noreply.github.com>
2025-04-02 14:02:56 -0300

    padronização

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({extended:true}))
 app.use(cors())
 
 app.get('/',(req,res)=>{
-    res.json({message:"API FUNCIONANDO!"})
+    res.json({message:"API is running."})
 })
 
 app.get('/test-db',async (req,res)=>{
@@ -19,8 +19,8 @@ app.get('/test-db',async (req,res)=>{
         const result = await db.query('select now()')
         res.json({sucess:true, serverTime:result.rows[0].now})
     }catch (err){
-        console.log('error ao executar a query.',err)
-        res.status(500).json({sucess:false, error:"Não foi possivel se conectar ao banco de dados."})
+        console.log('error executing query.',err)
+        res.status(500).json({sucess:false, error:"Unable to connect to the database. "})
     }
 })
 
